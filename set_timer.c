d25ac105bb072a2617c7c593a40c1ed9dd975f7d
朱少鹏 <zhushaopeng@zhushaopengs-iMac.local>
2018-07-06 21:05:24 +0800

    spzhu

--- set_timer.c
+++ set_timer.c
@@ -9,13 +9,13 @@
 #include <event2/event.h>
 #include <event2/event_struct.h>
 
-struct event ev;
+struct event *ev;
 struct timeval tv;
 
 void timer_cb(int fd, short event, void *argc)
 {
     printf("timer wake up\n");
-    event_add(&ev, &tv);    // reschedule timer
+    event_add(ev, &tv);    // reschedule timer
 }
 
 int main(int argc, char **argv)
@@ -24,11 +24,13 @@ int main(int argc, char **argv)
 
     tv.tv_sec = 5;
     tv.tv_usec = 0;
-    evtimer_set(&ev, timer_cb, NULL);
+//    evtimer_new(&ev, timer_cb, NULL);
 
-    event_base_set(base, &ev);
+    ev = event_new(base, -1, EV_PERSIST, timer_cb, NULL);
 
-    event_add(&ev, &tv);
+//    event_base_set(base, ev);
+
+    event_add(ev, &tv);
 
     
     event_base_dispatch(base);
